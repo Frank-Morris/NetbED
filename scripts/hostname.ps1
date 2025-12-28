17f389b65092e4f77e0d35eaebaf166c5815d06a
Frank-Morris <francismorris22@gmail.com>
2025-12-28 19:45:05 +0000

    Powershell script to set hostname on windows client

---
+++ scripts/hostname.ps1
@@ -0,0 +1,12 @@
+Write-Host Current host name is set to $env:COMPUTERNAME
+
+$hostname = "Client"
+
+if ($env:COMPUTERNAME -ne $hostname) {
+    Write-Host "Current name: $env:COMPUTERNAME"
+    Write-Host "Renaming computer name to '$hostname'..."
+    Rename-Computer -NewName $hostname -Force
+    Write-Warning "Hostname has been succesffully changed, a reboot is required for this to take affect..."
+}else{
+    Write-Host "Hostname is already correctly set to '$targetname'."
+}
